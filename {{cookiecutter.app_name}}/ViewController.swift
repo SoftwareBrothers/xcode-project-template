396f9c633acb2a80b17b5c87bdab83b7a7398b46
PiotrGorzelany-RST <piotr.gorzelany@rst-it.com>
2017-08-07 09:25:03 +0200

    Initial commit

---
+++ {{cookiecutter.app_name}}/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  {{cookiecutter.app_name}}
+//
+//  Created by PiotrGorzelanyMac on 07/08/2017.
+//  Copyright © 2017 rst-it. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
