9b723fa67e26125f8c11fa47e77d2f1af0647bc1
Piotr Gorzelany <piotr.gorzelany@rst-it.com>
2017-08-30 17:44:26 +0200

    Added LaurineGenerator and AlertPresenter

---
+++ {{cookiecutter.app_name}}/{{cookiecutter.app_name}}/Helpers/Protocols/ActivityPresenter.swift
@@ -0,0 +1,15 @@
+//
+//  ActivityPresenter.swift
+//  {{cookiecutter.app_name}}
+//
+//  Created by Piotr Gorzelany on 30.08.2017.
+//  Copyright © 2017 rst-it. All rights reserved.
+//
+
+import Foundation
+
+public protocol ActivityPresenter {
+    
+    func showActivityIndicator()
+    func hideActivityIndicator()
+}
