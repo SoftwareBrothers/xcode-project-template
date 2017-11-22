061204bf6e5cf7e99b53b4bb7ab35ad5b00e0852
Piotr Gorzelany <piotr.gorzelany@gmail.com>
2017-11-22 22:50:41 +0100

    Fixed SwiftLint warnings

--- {{cookiecutter.app_name}}/{{cookiecutter.app_name}}Tests/TestsAppDelegate.swift
+++ {{cookiecutter.app_name}}/{{cookiecutter.app_name}}Tests/TestsAppDelegate.swift
@@ -11,12 +11,12 @@ import UIKit
 /// The TestsAppDelegate used when the app is run in the test environment
 /// This AppDelegate usage is determined in the main.swift file in the main app target
 class TestsAppDelegate: UIResponder, UIApplicationDelegate {
-    
+
     var window: UIWindow?
     lazy var navigator = MainNavigator.shared
     lazy var deeplinkManager = DeeplinkManager.shared
     lazy var notificationsManager = NotificationsManager.shared
-    
+
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         //  Customize the TestsAppDelegate here
